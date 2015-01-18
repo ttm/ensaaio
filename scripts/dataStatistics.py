7c9b51e62e7b4981d32e49f0227b217046c19264
Renato Fabbri <renato.fabbri@gmail.com>
2015-01-17 23:29:56 -0200

    adicionado histograma de atividade do lm

--- scripts/dataStatistics.py
+++ scripts/dataStatistics.py
@@ -2,11 +2,13 @@
 #-*- coding: utf-8 -*-                                                 
 import rdflib as r
 from SPARQLWrapper import SPARQLWrapper, JSON
-import sys, time, cPickle as pickle
+import sys, time, cPickle as pickle, numpy as n, datetime
+import pylab as p
+import matplotlib.pyplot as pp
 T=time.time()
 ##############################
 f=open("./pickle/dicts.pickle", 'rb')
-um,ss=pickle.load(f)
+um,ss,shd=pickle.load(f)
 print(time.time()-T)
  
 print(u"there are %i AA sessions \n\
@@ -43,15 +45,95 @@ print(time.time()-T)
 # Alias Abitrarios sao resolvidos com o conhecimento
 # dos autores e apresentacao para a comunidade
 
-for aa in AA:
-    total=0
-    for nick in aa:
-        print('%s & %i'%(nick, len(um[nick]),))
-        total+=len(um[nick])
-    print total
+#for aa in AA:
+#    total=0
+#    for nick in aa:
+#        print('%s & %i'%(nick, len(um[nick]),))
+#        total+=len(um[nick])
+#    print total
+#
+#nicks=um.keys()
+#count=0
+#for nick in nicks:
+#    checkers=[i for i in um[nick] if i[-1]]
+#    count+=len(checkers)
+#
+f=open("./pickle/um_ss_.pickle", 'rb')
+um_,ss_,shd_=pickle.load(f)
+print(time.time()-T)
+### users in sessions
+# pega cada session, a ID do shout, vai no dict, pega o user.
+def S(nick):
+    for aa in AA:
+        if nick in aa:
+            return aa[0]
+    return nick
+users=[]
+#for seID in ss_.keys():
+#    if len(ss_[seID])>1:
+#        shouts=ss_[seID]
+#        for shout in shouts:
+#            shID=shout[-1]
+#            users+=[S(shd_[shID][1])]
+#nusers=len(set(users))
+## ve o len(set(users))
+#smsgs=[j for i in ss_.keys() for j in ss_[i]]
+#smsgs_=[j[0] for j in smsgs if j[0]]
+#smsgs_.sort()
+#
+#sessions=[
+#("number of sessions",len(ss_)),
+#("number of shouts in sessions",sum([len(ss_[i]) for i in ss_.keys()])),
+#("number sessions with more than 1 shout",sum([len(ss_[i])>1 for i in ss_.keys()])),
+#("number of shouts in sessions with more than 1 shout",sum([len(ss_[i]) for i in ss_.keys() if len(ss_[i])>1])),
+#("number of users in sessions",nusers),
+#("number of checkers in sessions",len(set([S(ss_[i][0][-2]) for i in ss_.keys() if len(ss_[i])>1]))),
+#("number of screencasts in sessions",len(set([i[2] for i in smsgs]))),
+#("number of scored sessions",sum([(ss_[i][0][3] not in ("","0")) for i in ss_.keys() if (len(ss_[i])>1)])),
+#("average session score",n.mean([int(ss_[i][0][3]) for i in ss_.keys() if (ss_[i][0][3] not in ("","0")) and (len(ss_[i])>1)])),
+#("standard deviation of score",n.std([int(ss_[i][0][3]) for i in ss_.keys() if (ss_[i][0][3] not in ("","0")) and (len(ss_[i])>1)])),
+#("average number of shouts",n.mean([len(ss_[i]) for i in ss_.keys() if len(ss_[i])>1])),
+#("standard deviation of number of shouts",n.std([len(ss_[i]) for i in ss_.keys() if len(ss_[i])>1])),
+##("first session from",min(smsgs_)),
+#("first session from",u'2011-07-06T03:23:05'), # smsgs_[211]
+##("last session from",max(smsgs_)),
+#("last session from",u'2014-04-01T09:11:36'),
+#]
+#i=0
+#for c in sessions:
+#    if i<8:
+#        print("%s & %i \\\\ \\hline"%(c[0],c[1]))
+#    elif i<12:
+#        print("%s & %.2f \\\\ \\hline"%(c[0],c[1]))
+#    else:
+#        print("%s & %s \\\\ \\hline"%(c[0],c[1]))
+#    i+=1
+#
+# t = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S')
+shs=[list(shd_[i])+[i] for i in shd_.keys()]
+shs_=[i[2] for i in shs]
+shs_.sort()
 
-nicks=um.keys()
-count=0
-for nick in nicks:
-    checkers=[i for i in um[nick] if i[-1]]
-    count+=len(checkers)
+shs__=[datetime.datetime.strptime((i,i[:i.find(".")])["." in i],'%Y-%m-%dT%H:%M:%S') for i in shs_[224:]]
+miny=shs__[0].year
+maxy=shs__[-1].year
+vals=[]
+for y in xrange(miny,maxy+1):
+    SH=[i for i in shs__ if i.year==y]
+    for m in xrange(1,12+1):
+        mc=sum([i.month==m for i in SH])
+        if m==1:
+            vals+=[(u"%i-%i"%(y,m),mc)]
+        else:
+            vals+=[(u"%i"%(m,),mc)]
+vals_=vals[6:-11]
+#fig = p.figure()
+#ax = fig.add_subplot(111)
+p1 = pp.bar(range(len(vals_)), [i[1] for i in vals_],   0.45)
+pp.xticks([i+0.45/2 for i in xrange(len(vals))],
+          [i[0] for i in vals_] , rotation=70)
+pp.xlim(-1,len(vals_))
+pp.ylim(0,max([i[1] for i in vals_])+200)
+pp.ylabel(u'messages count')
+pp.show()
+
--- scripts/dataSupport.py
+++ scripts/dataSupport.py
@@ -60,66 +60,68 @@ SELECT ?shID ?seID
 #pickle.dump(res,f)
 #f.close()
 #####################################################
-#f=open("./pickle/query2.pickle", 'rb')
-#res=pickle.load(f)
-#print(time.time()-T)
-# 
-#
-##users=[i["s"]["value"] for i in res]
-#nicks=[i["nick"]["value"] for i in res]
-#
-#user_msgs=um={}
-#sessions=ss={}
-#def getVal(tcoisa="coisa"):
-#    if tcoisa in sht.keys():
-#        tval=sht[tcoisa]["value"]
-#    else:
-#        tval=""
-#    return tval
-#G=getVal
-#countts=0
-#for sht in res:
-#    shID=sht["shID"]["value"]
-#    if "msg" in sht.keys():
-#        msg=sht["msg"]["value"]
-#    else:
-#        msg=""
-#    nick=sht["nick"]["value"]
-#    if "msg" in sht.keys():
-#        msg=sht["msg"]["value"]
-#    else:
-#        msg=""
-#    if "created" in sht.keys():
-#        created=sht["created"]["value"]
-#    else:
-#        created=""
-#    try:
-#        valid=sht["valid"]["value"]
-#    except:
-#        valid=False
-#
-#    if G("msg"):
-#        if G("nick") not in um.keys():
-#            um[G("nick")]=[] # lista de tuplas (msg, data)
-#        if G("msg") != u"TIMESLOT PERDIDO":
-#            um[G("nick")]+=[(G("msg"),G("created"),G("valid"),G("shID"))] # lista de tuplas (msg, data)
-#            if G("seID"):
-#                if G("seID") not in ss.keys():
-#                    ss[G("seID")]=[]
-#                ss[G("seID")]+=[(G("screated"), G("smsg"), G("screenscast"), G("sscore"),G("cnick"),G("shID"))]
-#        else:
-#            countts+=1
+f=open("./pickle/query2.pickle", 'rb')
+res=pickle.load(f)
+print(time.time()-T)
+ 
+
+#users=[i["s"]["value"] for i in res]
+nicks=[i["nick"]["value"] for i in res]
+
+shouts_dict=shd={}
+user_msgs=um={}
+sessions=ss={}
+def getVal(tcoisa="coisa"):
+    if tcoisa in sht.keys():
+        tval=sht[tcoisa]["value"]
+    else:
+        tval=""
+    return tval
+G=getVal
+countts=0
+for sht in res:
+    #shID=sht["shID"]["value"]
+    #if "msg" in sht.keys():
+    #    msg=sht["msg"]["value"]
+    #else:
+    #    msg=""
+    #nick=sht["nick"]["value"]
+    #if "msg" in sht.keys():
+    #    msg=sht["msg"]["value"]
+    #else:
+    #    msg=""
+    #if "created" in sht.keys():
+    #    created=sht["created"]["value"]
+    #else:
+    #    created=""
+    #try:
+    #    valid=sht["valid"]["value"]
+    #except:
+    #    valid=False
+    shd[G("shID")]=(G("msg"),G("nick"),G("created"),G("valid"),G("screated"), G("smsg"), G("screenscast"), G("sscore"),G("cnick"),G("shID"))
+
+    if G("msg"):
+        if G("nick") not in um.keys():
+            um[G("nick")]=[] # lista de tuplas (msg, data)
+        if G("msg") != u"TIMESLOT PERDIDO":
+            um[G("nick")]+=[(G("msg"),G("created"),G("valid"),G("shID"))] # lista de tuplas (msg, data)
+            if G("seID"):
+                if G("seID") not in ss.keys():
+                    ss[G("seID")]=[]
+                ss[G("seID")]+=[(G("screated"), G("smsg"), G("sscreencast"), G("sscore"),G("cnick"),G("shID"))]
+        else:
+            countts+=1
     #print("%s\n%s\n%s\n%s\n\n"%(nick, msg, created, valid))
     #time.sleep(0.3)
 
-#print(time.time()-T)
-#
-#f=open("./pickle/dicts.pickle", 'wb')
-#pickle.dump((um,ss),f)
-#f.close()
+print(time.time()-T)
+
+f=open("./pickle/dicts.pickle", 'wb')
+pickle.dump((um,ss,shd),f)
+f.close()
 ###############################
 f=open("./pickle/dicts.pickle", 'rb')
-um,ss=pickle.load(f)
+um,ss,shd=pickle.load(f)
 print(time.time()-T)
  
 print(u"there are %i AA sessions \n\
@@ -161,116 +163,116 @@ print(time.time()-T)
 #    ('angelina',),
 #    ('angelina',),
 #   ]
-#U=ums
-#AA=[
-#    (1,3,6,19,24,32,33,60,61,86,92,96,97,103,114,127,129,145,146,160,172), # hybrid
-#    (0,5,9,30,37,49,55,80,89,112,115,124,157,169,175), # humannoise
-#    (7,76,118,128,134,136), # angelina
-#    (4,13,16,45,93,178), # presto
-#    (10,47,87,99,142,153,173), # cravelho
-#    (21,111,117,119,154,159), # v1z
-#    (56,71,94,148,156), #audiohack
-#    (20,53,85,120), # Pjr (plinio)
-#    (48,77,108,125), # automata_
-#    (31,35,113), # anonymous, Guest
-#    (27,64), # ggdo
-#    (43,65), # witness
-#    (74,91), #sescBelenzinho
-#    (41,105), # tonussi
-#    (101,155), #gabithume
-#    (158,176), #liviaascava
-#    (54,162), #mancha
-#    (51,107), #glerm
-#    (58,130), #hiato(br1)
-#    (2,150), # adr
-#    (8,131), # yupana bot
-#    (122,), #DaneoShiga
-#    (78,), #jow
-#    (69,), #gonzo
-#    (11,), # cadinot
-#    (12,), # bzum
-#    (14,), # PedroBarata
-#    (57,), #flecha
-#    (15,), # joepie91
-#    (17,), # dj_brip
-#    (18,), # hick (nivaldinho)
-#    (22,), # doceafagonanuca
-#    (23,), # paulordbm
-#    (25,), # Aderbal
-#    (26,), # slucky
-#    (28,), # capo
-#    (29,), # teste
-#    (34,), # libotte
-#    (36,), # JPM
-#    (38,), # Manjaro
-#    (39,), # gilsonbeck 
-#    (40,), # monod
-#    (42,), # 'id found as shout creator but not as id for user with nick'
-#    (44,), # crash_daemons
-#    (46,), # SrKaioh
-#    (50,), #uriguilherme 
-#    (52,), #felipe machado
-#    (59,), #Rafaman
-#    (62,), #quirinobahr
-#    (63,), #tibiriba
-#    (66,), #leib999
-#    (67,), #bjonnh
-#    (68,), #kbsa
-#    (70,), #caioc
-#    (72,), #ricardo brazileiro
-#    (73,), #lari (larissa arruda?)
-#    (75,), #mquasar
-#    (79,), #atmt
-#    (81,), #queen
-#    (82,), #aurium
-#    (83,), #malacabado
-#    (84,), #leosimoes
-#    (88,), #eah
-#    (90,), #cibeleborg
-#    (95,), #kraven
-#    (98,), #rebecchi
-#    (100,), #lacRavoLenia
-#    (102,), #Mateus
-#    (104,), #coletivo
-#    (106,), #pliskin
-#    (109,), #hmbr
-#    (110,), #rck
-#    (116,), #sephioff8
-#    (121,), #fdeSanca
-#    (123,), #fran_paizao
-#    (126,), #ongueiro
-#    (132,), #yaso
-#    (133,), #casanova
-#    (135,), #gama
-#    (137,), #tarrafa
-#    (138,), #rfabbri bot
-#    (139,), #celularkobold
-#    (140,), #ananse
-#    (141,), #karmiac
-#    (143,), #fefo
-#    (144,), #gosma
-#    (147,), #lap01
-#    (149,), #paloma
-#    (151,), #barraponto
-#    (152,), #thibo
-#    (161,), #orlando o2
-#    (163,), #lmatos
-#    (164,), #isaura / fer
-#    (165,), #mari
-#    (166,), #lalenia
-#    (167,), #daniel teia
-#    (168,), #lucas
-#    (170,), #identi.ca
-#    (171,), #b0ttt
-#    (174,), #warlock
-#    (177,), #aleij
-#    (179,), #ispMarin
-#]
+U=ums
+AA=[
+    (1,3,6,19,24,32,33,60,61,86,92,96,97,103,114,127,129,145,146,160,172), # hybrid
+    (157,0,5,9,30,37,49,55,80,89,112,115,124,169,175), # humannoise
+    (76,7,118,128,134,136), # angelina
+    (45,4,13,16,93,178), # presto
+    (99,10,47,87,142,153,173), # cravelho
+    (21,111,117,119,154,159), # v1z
+    (56,71,94,148,156), #audiohack
+    (85,20,53,120), # Pjr (plinio)
+    (108,48,77,125), # automata_
+    (31,35,113), # anonymous, Guest
+    (64,27), # ggdo
+    (65,43), # witness
+    (74,91), #sescBelenzinho
+    (105,41), # tonussi
+    (101,155), #gabithume
+    (158,176), #liviaascava
+    (162,54), #mancha
+    (51,107), #glerm
+    (130,58), #hiato(br1)
+    (150,2), # adr
+    (8,131), # yupana bot
+    (122,), #DaneoShiga
+    (78,), #jow
+    (69,), #gonzo
+    (11,), # cadinot
+    (12,), # bzum
+    (14,), # PedroBarata
+    (57,), #flecha
+    (15,), # joepie91
+    (17,), # dj_brip
+    (18,), # hick (nivaldinho)
+    (22,), # doceafagonanuca
+    (23,), # paulordbm
+    (25,), # Aderbal
+    (26,), # slucky
+    (28,), # capo
+    (29,), # teste
+    (34,), # libotte
+    (36,), # JPM
+    (38,), # Manjaro
+    (39,), # gilsonbeck 
+    (40,), # monod
+    (42,), # 'id found as shout creator but not as id for user with nick'
+    (44,), # crash_daemons
+    (46,), # SrKaioh
+    (50,), #uriguilherme 
+    (52,), #felipe machado
+    (59,), #Rafaman
+    (62,), #quirinobahr
+    (63,), #tibiriba
+    (66,), #leib999
+    (67,), #bjonnh
+    (68,), #kbsa
+    (70,), #caioc
+    (72,), #ricardo brazileiro
+    (73,), #lari (larissa arruda?)
+    (75,), #mquasar
+    (79,), #atmt
+    (81,), #queen
+    (82,), #aurium
+    (83,), #malacabado
+    (84,), #leosimoes
+    (88,), #eah
+    (90,), #cibeleborg
+    (95,), #kraven
+    (98,), #rebecchi
+    (100,), #lacRavoLenia
+    (102,), #Mateus
+    (104,), #coletivo
+    (106,), #pliskin
+    (109,), #hmbr
+    (110,), #rck
+    (116,), #sephioff8
+    (121,), #fdeSanca
+    (123,), #fran_paizao
+    (126,), #ongueiro
+    (132,), #yaso
+    (133,), #casanova
+    (135,), #gama
+    (137,), #tarrafa
+    (138,), #rfabbri bot
+    (139,), #celularkobold
+    (140,), #ananse
+    (141,), #karmiac
+    (143,), #fefo
+    (144,), #gosma
+    (147,), #lap01
+    (149,), #paloma
+    (151,), #barraponto
+    (152,), #thibo
+    (161,), #orlando o2
+    (163,), #lmatos
+    (164,), #isaura / fer
+    (165,), #mari
+    (166,), #lalenia
+    (167,), #daniel teia
+    (168,), #lucas
+    (170,), #identi.ca
+    (171,), #b0ttt
+    (174,), #warlock
+    (177,), #aleij
+    (179,), #ispMarin
+]
 
-#AA=[tuple([U[ii] for ii in i]) for i in AA]
-#f=open("./pickle/arbitraryAlias.pickle", 'wb')
-#pickle.dump(AA,f)
-#f.close()
+AA=[tuple([U[ii] for ii in i]) for i in AA]
+f=open("./pickle/arbitraryAlias.pickle", 'wb')
+pickle.dump(AA,f)
+f.close()
 ###############################
 
 #
@@ -335,7 +337,7 @@ for nick in ums:
         #    exp.append(msg[0])
         
 
-countts=55831
+countts=59863
 countms=sum([len(um_[i]) for i in um_.keys()])
 counts=[
 (countts,"lost timeslot"),
@@ -351,3 +353,33 @@ counts=[
 for c in counts:
     print("%s & %i \\\\ \\hline"%(c[1],c[0]))
 print("total & %i "%(countts+ countms+ countsh+ countal+ countn+ countp+counta+counto+countms))
+
+# limpando ss e gravando objeto
+# todas as ids das msgs de um_
+
+# verifico cada session se tem msg valida, senao, descarto session.
+#msgs=[um_[i] for i in nicks]
+
+nicks=um_.keys()
+msgs=set([j[-1] for i in nicks for j in um_[i]])
+sss=ss.keys()
+ss_={}
+for sess in sss:
+    shouts=ss[sess]
+    shouts=[sh for sh in shouts if sh[-1] in msgs]
+    if shouts:
+        ss_[sess]=shouts
+
+shd_={}
+for mid in shd.keys():
+    if mid in msgs:
+        shd_[mid]=shd[mid]
+
+f=open("./pickle/um_ss_.pickle", 'wb')
+pickle.dump((um_,ss_,shd_),f)
+f.close()
+#####################################################
+f=open("./pickle/um_ss_.pickle", 'rb')
+um_,ss_,shd_=pickle.load(f)
+print(time.time()-T)
+ 
